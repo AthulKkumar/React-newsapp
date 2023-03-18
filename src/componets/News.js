7a6b841a6e870d1e50bda27aab38622e43bf61da
AthulKkumar <athulkkumar5@gmail.com>
2023-03-18 14:06:54 +0530

    Loading ui is added

--- src/componets/News.js
+++ src/componets/News.js
@@ -1,77 +1,57 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
-    articles = [
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-    ]
+
+    articles = []
 
     constructor() {
         super();
         this.state = {
             articles: this.articles,
+            loading: false,
             page: 1
         }
     }
 
     async componentDidMount() {
-        let url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=678889f8337b4812bb1deb5742be5402'
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=678889f8337b4812bb1deb5742be5402&page=${this.state.page}&pageSize=${this.props.pageSize}`
         let data = await fetch(url);
         let parsedData = await data.json();
         // console.log(parsedData);
-        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false })
 
     }
 
     handlePrevClick = async () => {
         console.log('Previous');
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=678889f8337b4812bb1deb5742be5402&page=${this.state.page - 1}&pageSize=20`
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=678889f8337b4812bb1deb5742be5402&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
+        this.setState({ loading: true })    
         let data = await fetch(url);
         let parsedData = await data.json();
         // console.log(parsedData);
         this.setState({
             page: this.state.page - 1,
-            articles: parsedData.articles
+            articles: parsedData.articles,
+            loading: false
         })
     }
 
     handleNextClick = async () => {
-        if ((this.state.page + 1 > Math.ceil(this.state.totalResults / 20))) {
+        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
 
-        } else {
+
             console.log('Next');
-            let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=678889f8337b4812bb1deb5742be5402&page=${this.state.page + 1}&pageSize=20`
+            let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=678889f8337b4812bb1deb5742be5402&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
+            this.setState({ loading: true })
             let data = await fetch(url);
             let parsedData = await data.json();
             // console.log(parsedData);
             this.setState({
                 page: this.state.page + 1,
-                articles: parsedData.articles
+                articles: parsedData.articles,
+                loading: false
             })
         }
     }
@@ -80,8 +60,9 @@ export class News extends Component {
         return (
             <div>
                 <div className='container'>
+                    {this.state.loading && <Spinner />}
                     <div className='row my-3'>
-                        {this.state.articles.map((element) => {
+                        {!this.state.loading && this.state.articles.map((element) => {
                             return <div className='col-md-4' key={element.url}>
                                 <NewsItem title={element.title ? element.title.slice(0, 45) : ''} description={element.description ? element.description.slice(0, 88) : ''} imageUrl={element.urlToImage} newsUrl={element.url} />
                             </div>
@@ -92,7 +73,7 @@ export class News extends Component {
                 </div>
                 <div className='container my-3 d-flex justify-content-between'>
                     <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-                    <button disabled={(this.state.page + 1 > Math.ceil(this.state.totalResults / 20))} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                    <button disabled={(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
