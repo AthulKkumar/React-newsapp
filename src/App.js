8016a89c97302d3ab2e586ff08e81839280114e9
AthulKkumar <athulkkumar5@gmail.com>
2023-03-20 12:10:16 +0530

    Replaced prev and next with infinite scroll

--- src/App.js
+++ src/App.js
@@ -10,19 +10,20 @@ import {
 } from "react-router-dom";
 
 export default class App extends Component {
+  pageSize = 6;
   render() {
     return (
         <div>
       <Router>
             <NavBar />
           <Routes>
-            <Route exact path="/" key={"general"} element={<News pageSize={5} country='in' category='general' />} />
-            <Route exact path="/business" key={"business"} element={<News pageSize={5} country='in' category='business' />} />
-            <Route exact path="/entertainment" key={"entertainment"} element={<News pageSize={5} country='in' category='entertainment' />} />
-            <Route exact path="/health"  key={"health"} element={<News pageSize={5} country='in' category='health' />} />
-            <Route exact path="/science" key={"science"} element={<News pageSize={5} country='in' category='science' />} />
-            <Route exact path="/sports" key={"sports"} element={<News pageSize={5} country='in' category='sports' />} />
-            <Route exact path="/technology" key={"technology"} element={<News pageSize={5} country='in' category='technology' />} />
+            <Route exact path="/" key={"general"} element={<News pageSize={this.pageSize} country='in' category='general' />} />
+            <Route exact path="/business" key={"business"} element={<News pageSize={this.pageSize} country='in' category='business' />} />
+            <Route exact path="/entertainment" key={"entertainment"} element={<News pageSize={this.pageSize} country='in' category='entertainment' />} />
+            <Route exact path="/health"  key={"health"} element={<News pageSize={this.pageSize} country='in' category='health' />} />
+            <Route exact path="/science" key={"science"} element={<News pageSize={this.pageSize} country='in' category='science' />} />
+            <Route exact path="/sports" key={"sports"} element={<News pageSize={this.pageSize} country='in' category='sports' />} />
+            <Route exact path="/technology" key={"technology"} element={<News pageSize={this.pageSize} country='in' category='technology' />} />
 
           </Routes>
       </Router>
--- src/componets/NewsItem.js
+++ src/componets/NewsItem.js
@@ -3,14 +3,18 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
     render() {
-        let { title, description, imageUrl, newsUrl } = this.props;
+        let { title, description, imageUrl, newsUrl, authour, date, source } = this.props;
         return (
             <div>
                 <div className="card" >
-                    <img src={imageUrl?imageUrl:'https://i.ytimg.com/vi/LBQNI1hoEU8/maxresdefault.jpg'} className="card-img-top" alt="..." />
+                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>
+                        {source}
+                    </span>
+                    <img src={imageUrl ? imageUrl : 'https://i.ytimg.com/vi/LBQNI1hoEU8/maxresdefault.jpg'} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
+                        <p className="card-text"><small className="text-muted">By {authour ? authour : 'Unknown'} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">Read more</a>
                     </div>
                 </div>
